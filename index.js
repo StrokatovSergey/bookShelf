9413234c1957a6ee59eda42bdbd50719e00a3153
Sergey Strokatov <strokatovsergey@gmail.com>
2019-11-18 17:00:22 +0400

    step3

--- index.js
+++ index.js
@@ -7,30 +7,69 @@ const pages = document.getElementById('pages')
 const bookName = document.getElementById('bookName')
 
 
-for (var i = 0; i < localStorage.length; i++){
-    let currentTask = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    const node = document.createElement("LI");
-    let author = document.createElement('SPAN');
-    let year = document.createElement('SPAN');
-    author.textContent = currentTask[0];
-    year.textContent = currentTask[1];
-    node.append(author,year)
-    ul.appendChild(node)
+
+const startWork = ()=>{
+    for (var i = 0; i < localStorage.length; i++) {
+        let currentTask = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        let nameOfClass = localStorage.key(i);
+        const node = document.createElement("LI");
+        node.classList.add(nameOfClass)
+
+        let author = document.createElement('SPAN');
+        let year = document.createElement('SPAN');
+        let deleteBtn = document.createElement('BUTTON');
+
+        deleteBtn.textContent = 'DELETE BOOK';
+        deleteBtn.classList.add(nameOfClass);
+        author.textContent = currentTask[0];
+        year.textContent = currentTask[1];
+
+        node.append(author, year, deleteBtn)
+        ul.appendChild(node)
+    }
+}
+const deleteAllList = ()=>{
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+}
+startWork();
+
+
+let deleteBtns = ul.getElementsByTagName('BUTTON');
+
+const deleteItem = (element) =>{
+    let deletedItem = element.className;
+    localStorage.removeItem(deletedItem)
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+
+    startWork()
+}
+
+for (const key in deleteBtns) {
+    if (deleteBtns.hasOwnProperty(key)) {
+        const element = deleteBtns[key];
+        element.addEventListener('click', () => deleteItem(element))
+    }
 }
 
 
-let totalTask = [];
 
-
-makeListItem = (authorInput, yearInput) =>{
+makeListItem = (authorInput, yearInput, className) =>{
 
     const node = document.createElement("LI");
+    // node.classList.add(className)
     let author = document.createElement('SPAN');
     let year = document.createElement('SPAN');
+    let deleteBtn = document.createElement('BUTTON');
+
     author.textContent = authorInput;
     year.textContent = yearInput;
+    deleteBtn.textContent = 'DELETE BOOK';
 
-    node.append(author,year)
+    node.append(author, year, deleteBtn)
     console.log(node);
     
     ul.appendChild(node)
@@ -41,10 +80,10 @@ makeListItem = (authorInput, yearInput) =>{
 form.addEventListener('submit', function(event){
     
     event.preventDefault()
-
-    makeListItem(author.value, year.value)
+    let ranId = Math.floor(Math.random() * 999);
+    makeListItem(author.value, year.value, ranId)
     let arrTasks = [];
-    let ranId = Math.floor(Math.random()*555);
+    
     arrTasks.push([author.value],[year.value])
     localStorage.setItem(ranId, JSON.stringify(arrTasks))
 })
